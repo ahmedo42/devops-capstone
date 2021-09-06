c44a5827cb5ef51bf7da124274dd9b29142a0683
ahmed <ahmed42795@gmail.com>
2021-09-06 14:42:20 +0200

    introduce lint error

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 @app.route("/")
 def home():
     html = "<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    return html.format(formats)
 
 @app.route("/predict", methods=['POST'])
 def predict():
